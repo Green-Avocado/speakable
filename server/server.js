e0b506e4804842cad6c2f551f6a87a71e05881bf
Green-Avocado <greenavocado@protonmail.com>
2021-11-14 14:30:39 -0800

    split into conferences

--- server/server.js
+++ server/server.js
@@ -14,21 +14,33 @@ function serverlog(req, code) {
     );
 }
 
-const submissions = [];
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() *
+ charactersLength));
+   }
+   return result;
+}
+
+const conferences = {};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/submit', function(req, res) {
-    form = {
+app.post('/create', function(req, res) {
+    conference = {
+        name: req.body.name,
         themes: req.body.themes,
         rooms: req.body.rooms,
         time: req.body.time,
-        votes: 0,
-    }
+        submissions: [],
+    };
 
-    for (let field in form) {
-        if (form[field] == null) {
+    for (let field in conference) {
+        if (conference[field] === null) {
             const res_code = 400;
             serverlog(req, res_code);
             res.status(res_code);
@@ -37,17 +49,84 @@ app.post('/submit', function(req, res) {
         }
     }
 
-    submissions.push(form);
+    let id;
+    do {
+        id = makeid(8);
+    } while (conferences[id] != null);
+
+    conferences[id] = conference;
 
     const res_code = 302;
     serverlog(req, res_code);
-    res.redirect('/');
+    res.redirect('/conference/' + id);
 });
 
-app.post('/vote', function(req, res) {
+app.get('join', function(req, res) {
+    id = req.body.id;
+
+    if (conferences[id] === null) {
+        const res_code = 400;
+        serverlog(req, res_code);
+        res.status(res_code);
+        res.json({error: "conference does not exist"});
+        return;
+    }
+
+    const res_code = 302;
+    serverlog(req, res_code);
+    res.redirect('/conference/' + id);
+});
+
+app.post('/conference/:conference/topic', function(req, res) {
+    id = req.params.conference;
+
+    if (conferences[id] === null) {
+        const res_code = 400;
+        serverlog(req, res_code);
+        res.status(res_code);
+        res.json({error: "conference does not exist"});
+        return;
+    }
+
+    topics = conferences[id].topics;
+
+    topic = {
+        topic: req.body.topic,
+        votes: 0,
+    };
+
+    for (let field in topic) {
+        if (topic[field] === null) {
+            const res_code = 400;
+            serverlog(req, res_code);
+            res.status(res_code);
+            res.json({error: "bad form data"});
+            return;
+        }
+    }
+
+    topics.push(topic);
+
+    const res_code = 302;
+    serverlog(req, res_code);
+    res.redirect('/conference/' + id);
+});
+
+app.post('/conference/:conference/vote', function(req, res) {
+    id = req.params.conference;
     idea = req.body.idea;
 
-    if (idea == null) {
+    if (conferences[conference] === null) {
+        const res_code = 400;
+        serverlog(req, res_code);
+        res.status(res_code);
+        res.json({error: "conference does not exist"});
+        return;
+    }
+
+    topics = conferences[id].topics;
+
+    if (idea === null) {
         const res_code = 400;
         serverlog(req, res_code);
         res.status(res_code);
@@ -55,8 +134,8 @@ app.post('/vote', function(req, res) {
         return;
     }
 
-    if (submissions[idea]) {
-        submissions[idea].votes += 1;
+    if (topics[idea]) {
+        topics[idea].votes += 1;
     } else {
         const res_code = 400;
         serverlog(req, res_code);
@@ -71,11 +150,55 @@ app.post('/vote', function(req, res) {
     res.json({message: "ok"});
 });
 
-app.get('/submissions', function(req, res) {
+app.get('/conference/:conference', function(req, res) {
+    id = req.params.conference;
+
+    if (conferences[id] === null) {
+        next();
+    }
+
+    topics = conferences[id].topics;
+
     const res_code = 200;
     serverlog(req, res_code);
     res.status(res_code);
-    res.json(submissions);
+    res.json(topics);
+});
+
+app.get('/conference/:conference/info', function(req, res) {
+    id = req.params.conference;
+
+    if (conferences[id] === null) {
+        const res_code = 400;
+        serverlog(req, res_code);
+        res.status(res_code);
+        res.json({error: "conference does not exist"});
+        return;
+    }
+
+    const res_code = 200;
+    serverlog(req, res_code);
+    res.status(res_code);
+    res.json(conferences[id]);
+});
+
+app.get('/conference/:conference/topics', function(req, res) {
+    id = req.body.conference;
+
+    if (conferences[id] === null) {
+        const res_code = 400;
+        serverlog(req, res_code);
+        res.status(res_code);
+        res.json({error: "conference does not exist"});
+        return;
+    }
+
+    topics = conferences[id].topics;
+
+    const res_code = 200;
+    serverlog(req, res_code);
+    res.status(res_code);
+    res.json(topics);
 });
 
 app.use(express.static('../public'));
