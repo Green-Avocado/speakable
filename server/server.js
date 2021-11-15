1c9a1baf95993df5ce20795966fc373919979ec5
Green-Avocado <greenavocado@protonmail.com>
2021-11-14 17:33:38 -0800

    reduced character set for conference ids

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ function serverlog(req, code) {
 
 function makeid(length) {
     var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() *
